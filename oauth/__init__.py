28bddce3ec2642026db8c1aabd774714f9301181
Alexander Richey <alexander.richey@gmail.com>
2020-09-19 13:53:07 -0700

    add favicon, error pages

--- oauth/__init__.py
+++ oauth/__init__.py
@@ -3,7 +3,16 @@ from starlette.applications import Starlette
 from starlette.routing import Route, Mount
 from starlette.staticfiles import StaticFiles
 
+from .templates import templates
 from .handlers import oauth_form, oauth, email_form, email
+
+
+async def not_found(request, exc):
+    return templates.TemplateResponse("404.html", {"request": request}, exc.status_code)
+
+
+async def server_error(request, exc):
+    return templates.TemplateResponse("500.html", {"request": request}, exc.status_code)
 
 
 app = Starlette(
@@ -15,4 +24,5 @@ app = Starlette(
         Route("/email", email, methods=["POST"]),
         Mount("/", app=StaticFiles(directory="static")),
     ],
+    exception_handlers={404: not_found, 500: server_error},
 )
